291ded51c5144a6e9d3ede212165f423149a02ae
root <divyajyotidas15@gmail.com>
2017-12-14 16:54:59 +0530

    Added New Feature and Fixrd Bug.

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,9 @@
-lass HelloWorld{
+public class HelloWorld{
 
 	public static void main(String[] args){
 		int i=0;
 		for(i=0;i<10;i++){
+			System.out.println("This is bug in this feature ...pls report");
 			System.out.println("\nHello World..."+i);
 
 		}
